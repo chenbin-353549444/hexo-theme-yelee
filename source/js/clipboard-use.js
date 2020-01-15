4bd5e2030d45e3188cdd33ba0fe045146396d5c9
chenbin <353549444@qq.com>
2020-01-15 15:09:17 +0800

    :sparkles: 优化复制代码按钮，添加消息弹出框

--- source/js/clipboard-use.js
+++ source/js/clipboard-use.js
@@ -4,14 +4,24 @@
     var initCopyCode = function(){
         var copyHtml = '';
         copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-        copyHtml += '  <i class="fa fa-clipboard"></i><span>●\'◡\'●点我复制</span>';
+        copyHtml += '  <i class="fa fa-clipboard" title="点我复制"></i>';
         copyHtml += '</button>';
         $(".highlight .code pre").before(copyHtml);
         new ClipboardJS('.btn-copy', {
             target: function(trigger) {
                 return trigger.nextElementSibling;
             }
+        }).on('success', function(e) {
+            $.gritter.add({
+                title: '消息',
+                text: '复制成功●\'◡\'●',
+                image: '/blog/img/avatar.png',
+                time: 1000,
+                speed:500,
+                sticky: false
+            });
+            e.clearSelection();
         });
-    }
+    };
     initCopyCode();
 }(window, document);
